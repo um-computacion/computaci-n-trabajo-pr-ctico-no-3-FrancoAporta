4722ea874c73af733bbd569797ac0edd8fee3a6a
Franco Aporta <f.aporta@alumno.um.edu.ar>
2025-04-16 11:03:34 -0300

    Implementación de pruebas para validación de números, ingreso de texto no numerico

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -14,3 +14,11 @@ class TestCalculoNumeros(unittest.TestCase):
     def test_ingreso_negativo(self, patch_input):
         with self.assertRaises(NumeroDebeSerPositivo):
             ingrese_numero()
+    
+    @patch('builtins.input', return_value='ABC')
+    def test_ingreso_letras(self, patch_input):
+        with self.assertRaises(ValueError):
+            ingrese_numero()
+
+if __name__ == '__main__':
+    unittest.main()
